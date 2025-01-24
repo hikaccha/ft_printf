10f09de0f1df11628b74bda5cca79cd20cdee581
Hikaru Ichikawa <hichikaw@c4r2s3.42tokyo.jp>
2025-01-24 23:11:52 +0900

    main.c was changed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hichikaw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 23:15:56 by hichikaw          #+#    #+#             */
-/*   Updated: 2024/12/12 02:13:42 by hichikaw         ###   ########.fr       */
+/*   Updated: 2025/01/24 19:24:31 by hichikaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,57 +16,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-int	main(void)
+#include <stdio.h>
+#include <limits.h>
+#include "ft_printf.h"
+
+void print_test(const char *title, int (*print_func)(const char *, ...))
 {
-	char	*s;
-	long	address1;
-	long	address2;
-	long	address3;
-	long	address4;
-	long	address5;
-	long	address6;
-	long	address7;
-	long	address8;
-	long	address9;
-	long	address10;
+    long values[] = {-1, 16, 17, LONG_MIN, LONG_MAX, INT_MIN, INT_MAX, ULONG_MAX, -ULONG_MAX, 0};
+    int i;
 
-	address1 = -1;
-	address2 = 16;
-	address3 = 17;
-	address4 = LONG_MIN;
-	address5 = LONG_MAX;
-	address6 = INT_MIN;
-	address7 = INT_MAX;
-	address8 = ULONG_MAX;
-	address9 = -ULONG_MAX;
-	address10 = 0;
-	printf("%s \n", "オリジナル");
-	printf("%p \n", address1);
-	printf("%p \n", address2);
-	printf("%p \n", address3);
-	printf("%p \n", address4);
-	printf("%p \n", address5);
-	printf("%p \n", address6);
-	printf("%p \n", address7);
-	printf("%p \n", address8);
-	printf("%p \n", address9);
-	printf("%p \n", address10);
-	printf("%s \n", "\n");
-
-	printf("%s \n", "俺のやつ");
-	ft_printf("%p \n", address1);
-	ft_printf("%p \n", address2);
-	ft_printf("%p \n", address3);
-	ft_printf("%p \n", address4);
-	ft_printf("%p \n", address5);
-	ft_printf("%p \n", address6);
-	ft_printf("%p \n", address7);
-	ft_printf("%p \n", address8);
-	ft_printf("%p \n", address9);
-	ft_printf("%p \n", address10);
-	printf("%s \n", "\n");
-
-	printf("%% \n");
-	ft_printf("%%\n");
-	return (0);
+    print_func("%s\n", title);
+    for (i = 0; i < 10; i++)
+        print_func("%p\n", (void *)values[i]);
+    print_func("\n");
 }
+
+int main(void)
+{
+    print_test("オリジナル", printf);
+    print_test("俺のやつ", ft_printf);
+
+    printf("%%\n");
+    ft_printf("%%\n");
+
+    return (0);
+}
+
